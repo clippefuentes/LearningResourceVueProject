1402a8069e2893d8e45c0db0e2c700dc671d8009
clippefuentes <clippefuentes@gmail.com>
2021-12-02 21:07:33 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+	<stored-resources
+		:resources="storedResources">
+	</stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resource/StoredResources.vue';
+export default {
+  components: {
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: '1',
+          title: 'Resource 1',
+          description: 'First Resources',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: '2',
+          title: 'Google',
+          description: 'Learn Google',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
